3e5f3e4edbb3c3a2693a3183c9f72e78495c6734
Chaitri <chaitri.s.mohan@gmail.com>
2021-01-13 09:07:35 +0530

    Fill Assignment: Manipulating Elements

--- block-BJaaeb/code/script.js
+++ block-BJaaeb/code/script.js
@@ -7,6 +7,15 @@ default value to be "text" and return the input element inside label. (create it
 
 // Your code goes here
 
+function createInputElm(label, type = 'text') {
+  let labelElm = document.createElement('label');
+  let labelText = document.createTextNode(label + ': ');
+  let inputElm = document.createElement('input');
+  inputElm.type = type;
+  labelElm.append(labelText, inputElm);
+  return labelElm;
+}
+
 // TEST
 createInputElm('Your name'); //<label>Your name: <input type="text"></label>
 createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></label>
@@ -15,6 +24,14 @@ createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></
 
 // Your code goes here
 
+function createInputElm(label, type = `text`) {
+  let labelElm = document.createElement('label');
+  let inputElm = document.createElement('input');
+  inputElm.type = `${type}`;
+  labelElm.append(`${label}:`, inputElm);
+  return labelElm;
+}
+
 // TEST
 createInputElm('Your name'); //<label>Your name: <input type="text"></label>
 createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></label>
@@ -22,6 +39,16 @@ createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></
 // 3. Create a function named `createList` that accept and array of data like ['Mango', 'Apple', 'Banana'] and returns
 // the html for the link like <ul> <li>Mango</li>  <li>Apple</li>  <li>Banana</li> </ul>
 // Your code goes here
+
+function createList(data) {
+  let ul = document.createElement('ul');
+  data.forEach(item => {
+    let li = document.createElement('li');
+    li.innerText = item;
+    ul.append(li);
+  });
+  return ul;
+}
 
 // TEST
 createList(['ALABAMA', 'ALASKA', 'HAWAII', 'KENTUCKY']);
@@ -38,6 +65,25 @@ createList(['Afghanistan', 'Antarctica', 'Congo', 'Estonia']);
   </li>
 </ul>
 */
+
+function createTodoList(toDo) {
+  let ul = document.createElement('ul');
+  toDo.forEach(item => {
+    let li = document.createElement('li');
+    let p = document.createElement('p');
+    p.innerText = item.name;
+    let input = document.createElement('input');
+    input.type = 'checkbox';
+    input.checked = true;
+    input.name = '';
+    input.id = '';
+    let span = document.createElement('span');
+    span.innerText = 'X';
+    li.append(p, input, span);
+    ul.append(li);
+  });
+  return ul;
+}
 
 // Your code goes here
 
